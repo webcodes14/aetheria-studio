49f1968b53496118b9d142e0270294f8f789354f
webcodes14 <semora.anton@gmail.com>
2026-02-11 22:44:23 +0100

    Theme Btn styling

--- src/components/ThemeBtn.jsx
+++ src/components/ThemeBtn.jsx
@@ -1,4 +1,7 @@
 import { useApp } from "../context/AppContext";
+import { motion } from "motion/react";
+
+import classes from "./ThemeBtn.module.css";
 
 import { FaSun } from "react-icons/fa";
 import { MdModeNight } from "react-icons/md";
@@ -10,12 +13,43 @@ const ThemeBtn = () => {
         setTheme((t) => t === 'light' ? 'dark' : 'light');
     }
 
-    return <button 
+    return <motion.button 
         type="button" 
+        className={classes.themeBtn}
+        style={{ justifyContent: theme === 'light' ? "flex-start" : "flex-end" }}
         onClick={handleTheme}
         aria-label="Doplnit vícero jazyků">
-            {theme === 'light' ? <FaSun /> : <MdModeNight />}
-    </button>
+            {theme === 'light' ? (
+                    <motion.span 
+                        className={`${classes.themeBtnCircle} ${classes.themeBtnLight}`}
+                        initial={{ opacity: 0, rotate: 0 }}
+                        animate={{ opacity: 1, rotate: 180 }}
+                        layout
+                        transition={{
+                            type: "spring",
+                            visualDuration: 0.3,
+                            bounce: 0.4,
+                        }}
+                    >
+                        <FaSun className="text-sm text-yellow-400" />
+                    </motion.span>
+                ) : (
+                    <motion.span 
+                        className={`${classes.themeBtnCircle} ${classes.themeBtnDark}`}
+                        initial={{ opacity: 0, rotate: 180 }}
+                        animate={{ opacity: 1, rotate: 360 }}
+                        layout
+                        transition={{
+                            type: "spring",
+                            visualDuration: 0.3,
+                            bounce: 0.4,
+                        }}
+                    >
+                        <MdModeNight className="text-sm text-white" />
+                    </motion.span>
+                )
+            }
+    </motion.button>
 }
 
 export default ThemeBtn;
